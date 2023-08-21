d50109232c456b8d67498324b7a77b87f2538165
guido gutierrez <121067148+guidoo25@users.noreply.github.com>
2023-08-21 17:50:14 -0500

    Update authRoute.js

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -8,20 +8,17 @@ const router = Router();
 router.post('/auth', async (req, res) => {
   const { email, password } = req.body;
 
-  // Query the database to find the user with the given email
   const [rows] = await pool.execute('SELECT * FROM users WHERE email = ?', [email]);
   const user = rows[0];
 
-  // If no user was found, return an error
   if (!user) {
     return res.status(401).json({ message: 'Incorrect email or password' });
   }
 
-  // Compare the provided password with the stored hashed password
   const isPasswordValid = await bcrypt.compare(password, user.password);
 
   if (isPasswordValid) {
-    // If the password is valid, create and return a JWT with the user ID, email, and token expiration time
+
     const token = jwt.sign(
       { userId: user.id, email: user.email },
       process.env.JWT_SECRET,
@@ -29,7 +26,6 @@ router.post('/auth', async (req, res) => {
     );
     return res.status(200).json({ message: 'Login satisfactorio', userId: user.id, token });
   } else {
-    // If the password is invalid, return an error
     return res.status(401).json({ message: 'Incorrectos email o password' });
   }
 });
